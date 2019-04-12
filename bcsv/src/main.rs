b7735356bb42e82b9efaf57405a7de1ddcc31265
Yang Keao <keao.yang@yahoo.com>
2019-04-12 22:43:53 +0800

    Add Process function

--- bcsv/src/main.rs
+++ bcsv/src/main.rs
@@ -1,26 +1,36 @@
 extern crate clap;
-use clap::{Arg, App};
-use std::io::{BufWriter, Read, BufReader};
+use clap::{App, Arg};
+use std::io::{BufReader, BufWriter, Read};
 
 mod bcsv;
-use bcsv::{encode, decode};
+use bcsv::process;
+
+const ENC_BLOCK_SIZE: usize = 1024 * 3 * 10;
+const DEC_BLOCK_SIZE: usize = 1024 * 5;
+
+const fn BCSV_LEN(len: usize) -> usize {
+    return len * 18;
+}
 
 fn main() {
     let matches = App::new("BaseCoreSocialistValues")
-                    .version("0.1.0")
-                    .author("YangKeao keao.yang@yahoo.com")
-                    .arg(Arg::with_name("decode")
-                         .short("d")
-                         .long("decode")
-                         .help("decode data"))
-                    .arg(Arg::with_name("FILE")
-                         .required(true))
-                    .about(r#"
+        .version("0.1.0")
+        .author("YangKeao keao.yang@yahoo.com")
+        .arg(
+            Arg::with_name("decode")
+                .short("d")
+                .long("decode")
+                .help("decode data"),
+        )
+        .arg(Arg::with_name("FILE").required(true))
+        .about(
+            r#"
 BaseCoreSocialistValues encode or decode FILE, or standard input, to standard output.
 
 With no FILE, or when FILE is -, read standard input.
-                          "#)
-                    .get_matches();
+                          "#,
+        )
+        .get_matches();
 
     let file_name = matches.value_of("FILE").unwrap();
 
@@ -39,8 +49,8 @@ With no FILE, or when FILE is -, read standard input.
     let writer = BufWriter::new(std::io::stdout());
 
     if matches.is_present("decode") {
-        decode(reader, writer)
+        //        process(reader, writer, unimplemented!(), input_buffer, output_buffer);
     } else {
-        encode(reader, writer)
+        //       process(reader, writer, unimplemented!(), input_buffer, output_buffer);
     }
 }

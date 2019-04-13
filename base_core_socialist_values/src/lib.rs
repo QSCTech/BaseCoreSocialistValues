f2d66514e56c93b4fecfa06d8ea7d762d77e1ee1
Yang Keao <keao.yang@yahoo.com>
2019-04-13 13:47:02 +0800

    Add Decoder

--- base_core_socialist_values/src/lib.rs
+++ base_core_socialist_values/src/lib.rs
@@ -20,6 +20,24 @@ lazy_static! {
     };
 }
 
+fn detect_order(str: &str) -> u8 {
+    let index1 = WORD_MAP.get(&str[0..1]).unwrap() - 0;
+    let index2 = WORD_MAP.get(&str[1..2]).unwrap() - 4;
+    let index3 = WORD_MAP.get(&str[2..3]).unwrap() - 8;
+
+    if index1 == 1 && index2 == 0 && index3 == 2 {
+        1
+    } else if index1 == 1 && index2 == 2 && index3 == 0 {
+        2
+    } else if index1 == 2 && index2 == 1 && index3 == 0 {
+        3
+    } else if index1 == 0 && index2 == 1 && index3 == 2 {
+        0
+    } else {
+        unreachable!()
+    }
+}
+
 struct Char {
     words: [&'static str; 3],
     order: u8,
@@ -33,6 +51,18 @@ impl Char {
                 WORD_SET[((byte >> 2) & 0b11) as usize],
                 WORD_SET[(4 + ((byte >> 4) & 0b11)) as usize],
                 WORD_SET[(8 + ((byte >> 6) & 0b11)) as usize],
+            ],
+        }
+    }
+
+    fn new_from_bcsv(bytes: [u8; 3]) -> Self {
+        let str = String::from_utf8(bytes.to_vec()).unwrap();
+        Self {
+            order: detect_order(&str),
+            words: [
+                &str[0..1],
+                &str[1..2],
+                &str[2..3]
             ],
         }
     }
@@ -62,6 +92,33 @@ impl Char {
             _ => unreachable!(),
         }
         Ok(24)
+    }
+
+    fn read_into(&self, mut writer: impl Write) -> io::Result<usize> {
+         match self.order {
+            0 => {
+                writer.write_all(&[*WORD_MAP.get(self.words[0]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[1]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[2]).unwrap() as u8]);
+            }
+            1 => {
+                writer.write_all(&[*WORD_MAP.get(self.words[1]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[0]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[2]).unwrap() as u8]);
+            }
+            2 => {
+                writer.write_all(&[*WORD_MAP.get(self.words[1]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[2]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[0]).unwrap() as u8]);
+            }
+            3 => {
+                writer.write_all(&[*WORD_MAP.get(self.words[2]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[1]).unwrap() as u8]);
+                writer.write_all(&[*WORD_MAP.get(self.words[0]).unwrap() as u8]);
+            }
+            _ => unreachable!(),
+        }
+        Ok(24)   
     }
 }
 
@@ -108,6 +165,10 @@ impl Buffer {
     pub fn reserve(&mut self, size: usize) {
         self.inner.reserve(size)
     }
+
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
 }
 
 pub struct Encoder {
@@ -143,6 +204,28 @@ impl Write for Encoder {
 
 impl Read for Encoder {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.output_data.read(buf)
+    }
+}
+
+impl Write for Decoder {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.input_buf.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.input_buf.flush()
+    }
+}
+
+impl Read for Decoder {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        while self.input_buf.len() > 3 {
+            let mut bytes = [0;3];
+            self.input_buf.read_exact(&mut bytes)?;
+
+            Char::new_from_bcsv(bytes).read_into(&mut self.output_data)?;
+        }
         self.output_data.read(buf)
     }
 }
